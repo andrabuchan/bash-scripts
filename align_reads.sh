cc888bc45a0ed2043a71eb4c9c42ed61401fbc2b
Andra Buchan <91843821+andrabuchan@users.noreply.github.com>
2025-03-20 10:06:53 -0400

    Update align_reads.sh

--- align_reads.sh
+++ align_reads.sh
@@ -31,8 +31,9 @@ else unzipped2=$file_b
 fi
 
 #aligns paired ends to reference db specified in first argument
+echo "Aligning $file_a_trimmed to $file_b_trimmed\n"
 bowtie2 -x $2 -1 $unzipped1 -2 $unzipped2 > $output_sam
-
+echo "Alignment complete! Saved to $output.sam\n"
 gzip $unzipped1 $unzipped2
 
 fi
